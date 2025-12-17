cd56c121994ea9225bf2dcd323e2f9997cafbd24
Junhong Liu <98734602+LJH-LBJ@users.noreply.github.com>
2025-12-17 15:20:08 +0800

    fix pre-commit
    Signed-off-by: Junhong <liujunhong11@huawei.com>

--- lm_service/entrypoints/openai/epd_api_server.py
+++ lm_service/entrypoints/openai/epd_api_server.py
@@ -22,7 +22,6 @@ from fastapi.responses import JSONResponse, StreamingResponse, PlainTextResponse
 from lm_service.entrypoints.cli_args import make_arg_parser
 from lm_service.apis.vllm.proxy import Proxy
 from lm_service.logger_utils import init_logger
-from lm_service.service_discovery import HealthCheckServiceDiscovery
 from vllm.entrypoints.openai.api_server import (
     validate_json_request,
     ChatCompletionRequest,
