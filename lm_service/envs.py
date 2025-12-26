5c8c4aff0a328824f18df13f95f89452f5de31ed
Junhong Liu <98734602+LJH-LBJ@users.noreply.github.com>
2025-12-26 17:13:51 +0800

    add parameter range check
    Signed-off-by: Junhong Liu <98734602+LJH-LBJ@users.noreply.github.com>

--- lm_service/envs.py
+++ lm_service/envs.py
@@ -6,6 +6,10 @@ from typing import Any, Callable
 
 from vllm.envs import env_with_choices
 
+from lm_service.logger_utils import init_logger
+
+logger = init_logger(__name__)
+
 _TRUE_VALUES = {"1", "true", "t", "y", "yes", "on"}
 
 # --8<-- [start:env-vars-definition]
@@ -70,6 +74,9 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "LM_SERVICE_REQUEST_TIMEOUT_SECONDS": lambda: int(
         os.getenv("LM_SERVICE_REQUEST_TIMEOUT_SECONDS", 120)
     ),
+    "LM_SERVICE_REQUEST_TIMEOUT_SECONDS": lambda: check_parameter_range(
+        "LM_SERVICE_REQUEST_TIMEOUT_SECONDS", 120, min_value=0
+    ),
     # Timeout in seconds for graceful worker shutdown and request cleanup.
     # Used to control how long a worker waits to finish processing before exiting.
     "LM_SERVICE_WORKER_GRACEFUL_EXIT_TIMEOUT_SEC": lambda: int(
@@ -85,3 +92,29 @@ def __getattr__(name: str):
     if name in environment_variables:
         return environment_variables[name]()
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+def check_parameter_range(
+        param_name: str,
+        default_value: Any,
+        min_value: int = None,
+        max_value: int = None
+):
+    try:
+        parameter = int(os.getenv(param_name, default_value))
+    except Exception:
+        logger.warning(
+            f"Environment variable {param_name} cannot be converted to int"
+        )
+        return
+
+    if min_value is not None:
+        if parameter < min_value:
+            logger.warning(
+                f"Environment variable {param_name}={parameter} is less than the minimum value {min_value}"
+            )
+            return
+    if max_value is not None:
+        if parameter > max_value:
+            logger.warning(
+                f"Environment variable {param_name}={parameter} is greater than the maximum value {max_value}"
+            )
